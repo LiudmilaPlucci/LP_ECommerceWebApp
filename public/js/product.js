0110dcec6f9642abae525ee0a858c735e45d0857
Liudmila Plucci <112190395+liuda33@users.noreply.github.com>
2024-07-02 20:34:15 +0100

    added cart, product

--- public/js/product.js
+++ public/js/product.js
@@ -13,7 +13,6 @@ productImages.forEach((item, i) => {
 })
 
 // toggle size buttons
-
 const sizeBtns = document.querySelectorAll('.size-radio-btn');
 let checkedBtn = 0;
 let size;
@@ -27,8 +26,11 @@ sizeBtns.forEach((item, i) => {
     })
 })
 
+//Show product's images and size buttons on page
 const setData = (data) => {
     let title = document.querySelector('title');
+    title.innerHTML += " ";
+    title.innerHTML += name.innerHTML = data.name;
 
     productImages.forEach((img, i) => {
         if (data.images[i]) {
@@ -45,12 +47,12 @@ const setData = (data) => {
             item.style.display = 'none';
         }
     })
-
-    title.innerHTML += " ";
-    title.innerHTML += name.innerHTML = data.name;
 }
 
+
 let productId = null;
+
+//fetch current product from db by ID
 products.on("value", function (snapshot) {
     if (!snapshot.exists()) {
         console.log("No products found");
@@ -59,15 +61,77 @@ products.on("value", function (snapshot) {
             let data = element.val();
             data.id = element.key.toString().replace("+ ", "").trim();
 
+            // if(currentProductId !== null && currentProductId !== 0) {
+            //     getProductById(data, currentProductId); // <-- function from realDBConnect
+            // }
+
             if (location.pathname !== '/product') {
                 productId = decodeURI(location.pathname.split('/').pop());
             }
             getProductById(data, productId);
-        })
+        });
     }
 })
 
-// get current product id
+// const validateForm = (checkedSizeId) => {
+//     if (currentProductId == null) {
+//         return showAlert('Product Id not found.');
+//     } else if (checkedSizeId == null || checkedSizeId === "") {
+//         return showAlert('Please select size.');
+//     }
+//     return true;
+// }
+
+//order
+let orderId = null;
+
+const createOrderId = function () {
+    const date = new Date();
+    console.log("date", date.getTime());
+
+    return date.getTime()
+}
+
+//fetch active order if exists OR create active order if no orders found in DB
+orders.on("value", function (snapshot) {
+    if (!snapshot.exists()) {
+        console.log("No orders found");
+        createOrder();
+    } else {
+        snapshot.forEach(function (element) {
+            let order = element.val();
+
+            if (order.active === "true") {
+                orderId = order.id;
+                console.log("Active order found: id = ", orderId);
+            }
+        })
+    }
+    if (orderId === null) {
+        createOrder();
+        console.log(" *******  inside null");
+    }
+})
+
+
+//create active order if NO ACTIVE orders found in DB
+function createOrder() {
+    console.log("orderId = ", orderId);
+
+    if (orderId === null) {
+        console.log("No active orders found.");
+
+        orderId = createOrderId().toString();
+        db
+            .ref('orders/' + orderId)
+            .set({
+                id: orderId.valueOf(),
+                active: "true"
+            });
+    }
+}
+
+//get current product id
 let currentProductId = getCurrentProductId();
 
 function getCurrentProductId() {
@@ -78,68 +142,11 @@ function getCurrentProductId() {
 
     return 0;
 }
-
 console.log(currentProductId);
 
-
-// order
-let orderId = null;
+//fetch productsIDs from active order OR create empty productsIds record in DB
 let productsInActiveOrder = "";
 
-const createOrderId = function () {
-    const date = new Date();
-    console.log("date", date.getTime());
-
-    return date.getTime()
-}
-
-// //fetch active order if exists OR create active order if no orders found in DB
-orders.on("value", function (snapshot) {
-    if (!snapshot.exists()) {
-        console.log("No orders found.");
-    } else {
-        snapshot.forEach(function (element) {
-            let data = element.val();
-            console.log("Existing Orders in DB: ", data);
-
-            let orders = data.id;
-            let ordersActive = data.active;
-            let tempID = [];
-
-            if (ordersActive !== null) {
-                for (let i = 0; i < ordersActive.length; i++){
-                    if (ordersActive[i]){
-                       tempID.push(orders[i]);
-                        console.log("!!!!!" + tempID);
-                    }
-                }
-                orderId = data.id.valueOf();
-                let active = data[orderId.active]
-                console.log("Active order found: id = ", orderId)
-                if (value.active){
-                    console.log("!!!!!!!!!!" + orderId);
-                }
-            }
-        })
-    }
-})
-
-// //create active order if NO ACTIVE orders found in DB
-if (orderId === null) {
-    console.log("No active orders found.");
-
-    orderId = createOrderId();
-    db
-        .ref('orders/' + orderId)
-        .set({
-            id: orderId.valueOf(),
-            active: true
-        });
-
-    console.log("New active order created: id = ", orderId);
-}
-
-// //fetch productsIDs from active order OR create empty productsIds record in DB
 productsIds.on("value", function (snapshot) {
     if (!snapshot.exists() && productsInActiveOrder === "") {
         console.log("No productsInOrder found");
@@ -160,107 +167,130 @@ productsIds.on("value", function (snapshot) {
     }
 })
 
+//посчитать количество товара для продукта, который собираемся добавить
+function countProductAmountInOrder(productId, checkedSizeId) {
+    const currentProductIdAndSize = productId + '-' + checkedSizeId;
+    console.log("currentProductIdAndSize = ", currentProductIdAndSize);
 
+    console.log("productsInActiveOrder = ", productsInActiveOrder, "  ", productsInActiveOrder.length);
 
-// function getOrderId() {
-//
-//     orderid.on("value", function (snapshot) {
-//         if (!snapshot.exists()) {
-//             console.log("No products found");
-//         } else {
-//             snapshot.forEach(function (element) {
-//                 let data = element.val();
-//                 data.id = element.key.toString().replace("+ ", "").trim();
-//                 if (location.pathname !== '/product') {
-//                     productId = decodeURI(location.pathname.split('/').pop());
-//                 }
-//                 getProductById(data, productId);
-//             })
-//         }
-//     })
-// }
-//
-// const validateForm = (checkedSizeId) => {
-//     if (productId === 0) {
-//         return showAlert('Product id not found.');
-//     } else if (checkedSizeId == null || checkedSizeId === "") {
-//         return showAlert('Please select a size.');
-//     }
-//     return true;
-// }
-//
-// let isOrderExisting;
-// const orderId = createOrderId();
-// orders.on("value", function (snapshot) {
-//     if (!snapshot.exists()) {
-//         console.log("No orders found");
-//         isOrderExisting = false;
-//         createNewOrder(orderId, new Date());
-//         isOrderExisting = true;
-//         console.log("****************** Order created ");
-//     } else {
-//         snapshot.forEach(function (element) {
-//             let data = element.val();
-//             if (data.active === true) {
-//                 isOrderExisting = true;
-//                 getOrderById(data, data.id);
-//             } else {
-//                 isOrderExisting = false;
-//                 createNewOrder(orderId, new Date());
-//                 isOrderExisting = true;
-//                 console.log("******************!!!!!!!!!! Order created ");
-//             }
-//         })
-//     }
-// })
-// const createOrderId = function () {
-//     const date = new Date();
-//     return date.getTime()
-// }
-//
-// const productsInOrder = [{productId, checkedSizeId, quantity}];
-// let totalPrice;
-//
-// function addProductToOrder(
-//     orderId, productsInOrder, totalPrice) {
-//     db
-//         .ref('orders/' + orderId)
-//         .set({
-//             products: productsInOrder,
-//             total: totalPrice.value
-//         });
-//     showAlert("Product added to cart successfully.");
-// }
-//
-// function createNewOrder(
-//     orderId, orderDate) {
-//     db
-//         .ref('orders/' + orderId)
-//         .set({
-//             id: orderId.value,
-//             date: orderDate.value,
-//             active:true,
-//         });
-//     showAlert("Order created successfully.");
-// }
-//
-// const addToCartBtn = document.querySelector("#addToCart");
-// addToCartBtn.addEventListener('click', function (e) {
-//
-//     const checkedSizeId = document.querySelector("label[class='size-radio-btn check']").id;
-//
-//     if (validateForm(checkedSizeId)) {
-//         loader.style.display = 'block';
-//         addProductToOrder(orderId, productsInOrder, totalPrice);
-//         location.reload();
-//     }
-// })
+    let productAmount;
+    if(productsInActiveOrder === null) {
+        productsInActiveOrder = "";
+        console.log("productsInActiveOrder was NULL and now is empty string = ", productsInActiveOrder);
+    } else if (productsInActiveOrder.length > 0) {
+        if (productsInActiveOrder.includes(currentProductIdAndSize)) {
+            productAmount = 1;
+        }
+    } else {
+        productAmount = 0;
+    }
 
+    console.log("productAmount, который будет добавлен в ордер = ", productAmount);
 
+    return productAmount;
+}
 
+function addProductToOrder(orderId, checkedSizeId, productId) {
+    let amount;
 
+    const productAmount = countProductAmountInOrder(productId, checkedSizeId);
 
+    let productRowName = productId + '-' + checkedSizeId;
 
+    if(productAmount === 0) {
+        amount = 1;
 
+        //Add new product in order
+        db
+            .ref('orders/' + orderId + '/products/' + productRowName)
+            .set({
+                id: productRowName,
+                productId: productId,
+                checkedSizeId: checkedSizeId,
+                amount: amount
+            });
 
+        productsInActiveOrder = productsInActiveOrder + " " + productRowName;
+        db
+            .ref('productsIds/')
+            .set({
+                ids: productsInActiveOrder
+            });
+
+    } else if(productAmount === 1) {
+
+        orders.on("value", function (snapshot) {
+            if (!snapshot.exists()) {
+                console.log("No orders found");
+            } else {
+                snapshot.forEach(function (element) {
+                    let dataOrders = element.val();
+                    const products = dataOrders.products;
+
+                    for (let item in products) {
+                        amount = products[productRowName].amount;
+                    }
+                })
+            }
+
+        })
+
+        amount = amount + 1;
+
+        //Update amount
+        db
+            .ref('orders/' + orderId + '/products/' + productRowName)
+            .set({
+                id: productRowName,
+                productId: productId,
+                checkedSizeId: checkedSizeId,
+                amount: amount
+            });
+
+        orders.on("value", function (snapshot) {
+            if (!snapshot.exists()) {
+                console.log("No orders found");
+            } else {
+                snapshot.forEach(function (element) {
+                    let newDataOrders = element.val();
+                    const products = newDataOrders.products;
+
+                    console.log("newDataOrders = ", newDataOrders);
+
+                })
+            }
+        })
+
+        productsIds.on("value", function (snapshot) {
+            if (!snapshot.exists()) {
+                console.log("No productsInOrder found");
+            } else {
+                snapshot.forEach(function (element) {
+                    let newProductsIds = element.val();
+
+                    console.log("newProductsIds = ", newProductsIds);
+
+                });
+            }
+        })
+    }
+}
+
+const addToCartBtn = document.querySelector("#addToCart");
+
+addToCartBtn.addEventListener('click', function (e) {
+    const checkedSizeId = document.querySelector("label[class='size-radio-btn check']").id;
+
+    let productInOrder;
+
+    // if (validateForm(checkedSizeId)) {
+
+    addProductToOrder(orderId, checkedSizeId, currentProductId);
+    // }
+    // loader.style.display = 'block';
+
+    //location.reload();
+    // }
+})
 
